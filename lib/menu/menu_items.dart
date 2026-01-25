dfd08f559134b63a1fd0e8e6d7d97cffe213fd92
Asahel Ocaño <57965460+asaocano@users.noreply.github.com>
2026-01-24 17:08:42 -0700

    Text routes

--- lib/menu/menu_items.dart
+++ lib/menu/menu_items.dart
@@ -24,7 +24,7 @@ const appMenuItems = <MenuItem>[
   MenuItem(
     title: 'Tarjetas',
     subtitle: 'un contenedor estilizado',
-    link: '/card',
+    link: '/cards',
     icon: Icons.credit_card,
   ),
 ];
